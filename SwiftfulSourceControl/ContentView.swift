4e9064e10b94298c89686b386e40398d4c6f63b6
Oleksii Ruban <lierival@gmail.com>
2024-10-18 13:01:25 +0200

    Add click me button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("SwiftUI Thinking!")
 
+            Button("Click me!") {
+                
+            }
         }
         .padding()
     }
