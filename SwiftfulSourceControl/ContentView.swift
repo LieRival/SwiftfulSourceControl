134c1d001db143566a9a586a20da9bd863f5897d
Oleksii Ruban <lierival@gmail.com>
2024-10-20 15:28:17 +0200

    [Update] text swiftUI

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font (.largeTitle)
                 .foregroundStyle(.tint)
-            Text("SwiftUI Thinking!")
+            Text("SwiftUI!")
 
             Button("Subscribe") {
                 
