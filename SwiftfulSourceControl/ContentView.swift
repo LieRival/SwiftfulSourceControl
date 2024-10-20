d13c1b128fe50a24a21ce7759535560ecce251cd
Oleksii Ruban <lierival@gmail.com>
2024-10-20 15:21:30 +0200

    [Future] Rename sibscribe button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("SwiftUI Thinking!")
 
-            Button("Click me!") {
+            Button("Subscribe") {
                 
             }
         }
